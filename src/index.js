9c3b1daa6b92d042854cd51e578a1b6349982e73
github username <github email address>
2019-09-04 10:22:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+import Login from './login'
+import About from './about'
+
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () => {
+  return (
+  <div>
+    <NavLink to="/" style={link} activeStyle={{background: 'magenta'}} >Home</NavLink>
+    <NavLink to="/about" style={link} activeStyle={{background: 'darkblue' }}>About</NavLink>
+    <NavLink to="/login" exact style={link} activeStyle={{background: 'darkblue' }}>Login</NavLink>
+  </div>
+  )// Taking out the "exact" here and below in the router does not seem to have an effect wrt the secondary links
+}
 
 const Home = () => {
   return (
@@ -9,7 +32,22 @@ const Home = () => {
   );
 };
 
+const AnythingElse = () => {
+  return (
+    <h5>Anything Else</h5>
+  )
+}
+
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route path='/' render={Home}/>
+      <Route path='/else' render={AnythingElse}/>
+      <Route path='/about' render={About}/>
+      <Route exact path='/login' component={Login}/>
+    </React.Fragment>
+  </Router>,
+  // <Home />,
   document.getElementById('root')
 );
